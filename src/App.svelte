fc530c32ab5c71b23f164b22f47cd9a308a03628
simmedia <simadurlan92@gmail.com>
2020-04-05 21:50:00 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+  import { Router, Route } from "svelte-routing";
+
+  import Header from "./components/Header.svelte";
+  import Home from "./pages/Home.svelte";
+  import About from "./pages/About.svelte";
+  import Contact from "./pages/Contact.svelte";
+
+  export let url = "";
+</script>
+
+<style>
+  :global(*),
+  :global(body) {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    color: #444;
+  }
+
+  .container {
+    padding: 30px;
+    max-width: 1366px;
+    margin: auto;
+  }
+</style>
+
+<Header />
+<div class="container">
+  <Router {url}>
+    <Route path="/" component={Home} />
+    <Route path="about" component={About} />
+    <Route path="contact" component={Contact} />
+  </Router>
+</div>
---
+++ src/pages/Home.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { links } from "svelte-routing";
+  let users = [
+    { name: "Stefan", age: 28 },
+    { name: "Milica", age: 27 },
+    { name: "Petar", age: 26 }
+  ];
+</script>
+
+<ul>
+  {#each users as user}
+    <li>
+      {user.name} , {user.age}
+      <a use:links >View</a>
+    </li>
+  {/each}
+</ul>
